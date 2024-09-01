a1f03f889e6abf434850b31fed28de0133f10f24
Niag2021 <arnacibia.guerrero.nicolas@gmail.com>
2024-08-31 20:48:29 -0400

    En servidor registrarse/sign up, inicializaciones en initialSetup.js, creacion modelo re Roles y rutas

--- client/src/controllers/auth.controllers.js
+++ client/src/controllers/auth.controllers.js
@@ -1,16 +1,36 @@
 import User from '../models/User.js';
+import jwt from 'jsonwebtoken';
+import config from '../config.js';
+import Role from '../models/Role.js';
 
 export const signUp = async (req, res) => {
+    //Registro de usuario.
     const {username, email, password, roles} = req.body;
 
     const newUser = new User({
         username,
         email, 
-        password: User.encryptPassword(password)
+        password: await User.encryptPassword(password)
     })
-    console.log(newUser)
 
-    res.json('signup');    
+    if(roles){
+        const foundRoles = await Role.find({name: {$in: roles}})
+        newUser.roles = foundRoles.map(role => role._id)
+    }else{
+        const role = await Role.findOne({name: "user"})
+        newUser.roles = [role._id];
+    }
+
+    //Para guardarlo. 
+    const savedUser = await newUser.save();
+    console.log(savedUser)
+
+    const token = jwt.sign({id: savedUser._id}, config.SECRET, {
+        expiresIn: 86400 //En segundo son 24 horas.
+    })
+
+    res.status(200).json({token})
+    //res.json('signup'); 
 }
 
 export const signIn = async (req, res) => {
