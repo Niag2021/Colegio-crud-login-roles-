94c1b75f3172d6cd7ec0b9c725cdf95fb6ee11fc
Niag2021 <arnacibia.guerrero.nicolas@gmail.com>
2024-09-02 22:22:58 -0400

    Rutas de products, validacion de token, perfil moderador y administrador

--- client/src/routes/products.routes.js
+++ client/src/routes/products.routes.js
@@ -2,11 +2,15 @@ import { Router } from "express";
 const router = Router()
 
 import * as productsCtrl from '../controllers/products.controllers.js';
+import { authJwt } from "../middlewares/index.js";
+//import { authJwt } from "../middlewares/authJwt.js";
+//import { verifyToken, isModerator, isAdmin } from "../middlewares/authJwt.js";
 
-router.post('/', productsCtrl.createProduct);
+//router.post('/', verifyToken, isModerator, productsCtrl.createProduct);
+router.post('/', [authJwt.verifyToken, authJwt.isModerator], productsCtrl.createProduct);
 router.get('/', productsCtrl.getProducts);
 router.get('/:productId', productsCtrl.getProductById);
-router.put('/:productId', productsCtrl.updateProductById);
-router.delete('/:productId', productsCtrl.deleteProductById);
+router.put('/:productId', [authJwt.verifyToken, authJwt.isAdmin], productsCtrl.updateProductById);
+router.delete('/:productId', [authJwt.verifyToken, authJwt.isAdmin],productsCtrl.deleteProductById);
 
 export default router;
